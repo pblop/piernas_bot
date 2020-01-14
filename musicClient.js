e62f3b92ab89123ded5c9979a74459febc9a63ca
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2020-01-14 19:17:34 +0100

    Working 🔧

--- musicClient.js
+++ musicClient.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js')
+const { musicPrefix: prefix } = require('./config.json')
 const client = new Discord.Client()
+const ytdl = require('ytdl-core')
 
 client.on('ready', () => {
   console.log(`Music bot logged in as ${client.user.tag}!`)
@@ -9,9 +11,72 @@ client.on('ready', () => {
 })
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong.')
+  if (!msg.content.startsWith(prefix) || msg.author.bot || msg.member == null)
+    return
+  const args = msg.content
+    .slice(prefix.length)
+    .trim()
+    .split(' ')
+  const command = args.shift().toLowerCase()
+
+  if (command === 'play') {
+    if (args.length < 1) {
+      console.log(
+        `Tienes que usar el comando seguido de una canción: "${prefix}play <canción>"`
+      )
+    } else {
+      playSong(msg, args[0])
+    }
+  } else if (command === 'join') {
+    joinChannelCommand(msg)
+  } else if (command === 'leave') {
+    leaveChannelCommand(msg)
+  } else {
+    msg.reply(`Unknown command "${command}"`)
   }
 })
+
+function playSong (msg, link) {
+  const streamOptions = { seek: 0, volume: 1 }
+
+  const connection = getVoiceConnectionFromMessage(msg)
+  if (connection == null) {
+    msg.reply('No estoy en un canal de voz')
+    return
+  }
+
+  try {
+    connection.play(
+      ytdl(link, {
+        filter: 'audioonly'
+      }),
+      streamOptions
+    )
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function joinChannelCommand (msg) {
+  const vc = msg.member.voice.channel
+  if (vc != null) {
+    if (vc.joinable) {
+      vc.join()
+    } else {
+      msg.reply('No me tengo permisos para entrar en tu canal de voz')
+    }
+  } else {
+    msg.reply('No estás en un canal de voz')
+  }
+}
+
+function getVoiceConnectionFromMessage (msg) {
+  const guild = msg.guild
+  return client.voice.connections.find(vc => vc.channel.guild === guild)
+}
+
+function leaveChannelCommand (msg) {
+  getVoiceConnectionFromMessage(msg).disconnect()
+}
 
 module.exports = client
